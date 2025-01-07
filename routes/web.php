68acf48c32a4adc3c3c94e9c32d4ccaa5e0dcc9c
Jose Marcio Barthem <marciodeveloper@outlook.com>
2025-01-07 12:29:10 -0300

    feat: autenticacao e registro

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\StoreAuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 // Sobudomain Routes Stores
@@ -15,11 +16,19 @@ Route::domain('{subdomain}.localhost')->group(function(){
         Route::get('cancel', [\App\Http\Controllers\Front\CartController::class, 'cancel'])->name('cancel');
     });
 
-    Route::prefix('checkout')/*>middleware('auth.store')*/->name('checkout.')->group(function(){
+    Route::prefix('checkout')->middleware([StoreAuthMiddleware::class])->name('checkout.')->group(function(){
         Route::get('/', [\App\Http\Controllers\Front\CheckoutController::class, 'checkout'])->name('checkout');
         Route::post('/proccess', [\App\Http\Controllers\Front\CheckoutController::class, 'proccess'])->name('proccess');
         Route::get('/thanks', [\App\Http\Controllers\Front\CheckoutController::class, 'thanks'])->name('thanks');
     });
+
+    Route::name('sign.')->group(function() {
+        Route::get('/sign-in', [\App\Http\Controllers\Front\AuthenticateController::class, 'index'])->name('index');
+        Route::post('/sign-in', [\App\Http\Controllers\Front\AuthenticateController::class, 'signIn'])->name('in');
+        Route::post('/sign-up', [\App\Http\Controllers\Front\AuthenticateController::class, 'signUp'])->name('up');
+    });
+    Route::get('logout', [\App\Http\Controllers\Front\AuthenticateController::class, 'logout'])->name('up');
+
 });
 
 Route::get('/', function () {
